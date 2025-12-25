b878284bc17193ef61c74fe2677cb76cbc0388a9
Rahul-5555 <rahul.malviya676@gmail.com>
2025-12-25 22:04:42 +0530

    adding linear search question

---
+++ 4.Recursion/R9.js
@@ -0,0 +1,15 @@
+// Linear search
+
+let arr = [2,4,6,7,9,3];
+
+function linearSearch(arr,target){
+  for(let i=0; i<arr.length; i++){
+    if(arr[i] == target){
+      return i;
+    }
+  }
+  return -1;
+}
+
+let result = linearSearch(arr,7)
+console.log(result)
