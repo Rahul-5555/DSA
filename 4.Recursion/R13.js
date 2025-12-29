c0b330b139473c861e6782a5ac06e6bc59cd8a5d
Rahul-5555 <rahul.malviya676@gmail.com>
2025-12-29 23:40:41 +0530

    adding insertion  sort algoirithm

---
+++ 4.Recursion/R13.js
@@ -0,0 +1,22 @@
+// Insertion Sort Algorithm in JavaScript?
+
+let arr = [7, 1, 5, 12, -10, 0, 4, 3, 2];
+
+function insertionSort(arr) {
+  let n = arr.length;
+
+  for (let i = 1; i < n; i++) {
+    let curr = arr[i];
+    let prev = i - 1;
+
+    while(arr[prev] > curr && prev >= 0){
+      arr[prev + 1] = arr[prev];
+      prev--;
+    }
+    arr[prev + 1] = curr;
+  }
+  return arr;
+}
+
+let result = insertionSort(arr);
+console.log(result)
