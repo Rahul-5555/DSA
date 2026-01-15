8e7877e7fedc9afa1af95fef4f11e86244ffed79
Rahul-5555 <rahul.malviya676@gmail.com>
2026-01-15 12:09:40 +0530

    Adding lengthOfTheLastWord Ques. third approach

---
+++ 7.String/Q3.js
@@ -0,0 +1,20 @@
+// Approach 3: 
+var lengthOfLastWord = function (s) {
+  let n = s.length - 1;
+
+  let count = 0;
+  while (n >= 0) {
+    if (s[n] != " ") {
+      ++count;
+    }
+    else if (count > 0) {
+      break;
+    }
+    --n;
+  }
+  return count;
+}
+
+
+let s = "Hello World   ";
+console.log(lengthOfLastWord(s)); // Output: 5
