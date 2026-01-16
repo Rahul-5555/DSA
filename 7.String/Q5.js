000d7272dbae6b6f45159c75cdb2b87cbc1d51c5
Rahul-5555 <rahul.malviya676@gmail.com>
2026-01-16 10:12:27 +0530

    Jwels ans stones problem using inbuilt method?

---
+++ 7.String/Q5.js
@@ -0,0 +1,12 @@
+// Jwels ans stones problem using inbuilt method?
+
+var numJewelsInStones = function (jewels, stones) {
+
+  let count = 0;
+  for (let i = 0; i < stones.length; i++) {
+    if (jewels.includes(stones[i])) {
+      ++count;
+    }
+  }
+  return count;
+}
