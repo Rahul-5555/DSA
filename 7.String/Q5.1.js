eacb525ff637c3176f0160e79c403b26718e0f4e
Rahul-5555 <rahul.malviya676@gmail.com>
2026-01-16 10:50:18 +0530

    jewels and stone without inbuilt method

---
+++ 7.String/Q5.1.js
@@ -0,0 +1,21 @@
+//Approach 2: Jwels ans stones problem without using inbuilt method?
+
+var numJewelsInStones = function (jewels, stones) {
+
+  let count = 0;
+  for (let i = 0; i < stones.length; i++) {
+    // if (jewels.includes(stones[i])) {
+    //   ++count;
+    // }
+    for (j = 0; j < jewels.length; j++){
+      if(jewels[j] === stones[i]){
+        ++count;
+      }
+    }
+  }
+  return count;
+}
+// it is brute force approach
+// m = length of jewels
+// n = length of stone
+// TC: O(m*n)  SC: O(1)
