b872c6de7eb88629da937b003400a0eaa09fa2b3
Rahul-5555 <rahul.malviya676@gmail.com>
2026-01-18 09:58:53 +0530

    Add Split a string in balanced string

--- 7.String/Q7.js
+++ 7.String/Q7.js
@@ -1,1 +1,22 @@
-// 
+// leetcode:1221: split a string in a balanced string?
+
+var balamcedstringSplit = function (s) {
+  let R = 0;
+  let L = 0;
+  let count = 0;
+  for(let i=0; i< s.length; i++){
+    if(s[i] === "R"){
+      ++R;
+    }
+    else {
+      ++L;
+    }
+    if(L === R){
+      ++count;
+      R = 0;
+      L = 0;
+    }
+  }
+  return count;
+}
+
