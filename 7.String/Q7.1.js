f2089ab1d795e4e920d31fb5de063df160c25f25
Rahul-5555 <rahul.malviya676@gmail.com>
2026-01-18 10:07:55 +0530

    add improve version of split a string in balanced string

---
+++ 7.String/Q7.1.js
@@ -0,0 +1,19 @@
+// Improve solution leetcode 1221: Split a string in balanced string?
+
+var balancedStringSplit = function (s) {
+  let temp = 0;
+  let count = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "R") {
+      ++temp;
+    }
+    else {
+      --temp;
+    }
+
+    if (temp === 0) {
+      ++count;
+    }
+  }
+  return count;
+}
