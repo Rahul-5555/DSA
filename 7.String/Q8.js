4967b39ea0937e40657b6af9e56532a85330673b
Rahul-5555 <rahul.malviya676@gmail.com>
2026-01-19 09:55:02 +0530

    adding reverse string 2

--- 7.String/Q8.js
+++ 7.String/Q8.js
@@ -1,14 +1,21 @@
 // leetcode 541: Reverse String 2
 
 var reverseStr = function (s, k) {
-  console.log(s)
-  s = s.split("");
-  console.log(s)
-  s = s.join("");
-  console.log(s)
-  return
+  s = s.split(""); // ["h","e","l","l","o"]
+
+  for (let x = 0; x < s.length; x = x + (2 * k)) {
+    // reverse the first k element starting from x
+    let n = k;
+    let mid = Math.floor(n / 2);
+    for (let i = 0; i < mid; i++) {
+      let temp = s[x + i];
+      s[x + i] = s[x + n - 1 - i];
+      s[x + n - 1 - i] = temp;
+    }
+  }
+  return s.join("")
 }
 
-let s = "hello"
+// let s = "hello"
 // console.log(s)
-reverseStr(s, 2)
+// reverseStr(s, 2)
