b1e6f56cb057eb28e6273518695e06afb8d807ac
Rahul-5555 <rahul.malviya676@gmail.com>
2026-01-27 09:37:10 +0530

    add is valid anagram approach 1

--- 7.String/Q12.js
+++ 7.String/Q12.js
@@ -1,1 +1,10 @@
-// finding valid anagram
+// Leetcode 242: Valid Anagram
+
+var isAnagram = function (s, t) {
+  return s.split("").sort().join("") === t.split("").sort().join("");
+};
+
+let s = "anagram";
+let t = "nagaram";
+
+console.log(isAnagram(s, t)); // true
