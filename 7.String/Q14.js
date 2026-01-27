a6b75c3d1a4fa79d2ec30d491126e9a02c2fd72e
Rahul-5555 <rahul.malviya676@gmail.com>
2026-01-27 17:31:53 +0530

    add group anagram quesion Q14.js (leetcode:49)

---
+++ 7.String/Q14.js
@@ -0,0 +1,19 @@
+// Leetcode 49 : Group anagram
+
+var groupAnagram = function (strs) {
+  let map = {};
+
+  for (let i = 0; i < strs.length; i++) {
+    let sortedStr = strs[i].split('').sort().join('');
+
+    if (!map[sortedStr]) {
+      map[sortedStr] = [strs[i]];
+    }
+    else {
+      map[sortedStr].push(strs[i]);
+    }
+  }
+  return [...Object.values(map)];
+}
+
+console.log(groupAnagram(["eat", "tea", "tan", "ate", "nat", "bat"]));
