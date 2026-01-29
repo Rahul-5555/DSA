bea8ad24826a10c42ff658b871a4b9d8c954286d
Rahul-5555 <rahul.malviya676@gmail.com>
2026-01-29 11:12:07 +0530

    modified Q14.js

--- 7.String/Q14.js
+++ 7.String/Q14.js
@@ -1,4 +1,11 @@
 // Leetcode 49 : Group anagram
+// Approach 1: Using Sorting and Hash Map
+// 1. Create a map to store groups of anagrams
+// 2. For each string in the input array:
+//    a. Sort the characters of the string to form a key
+//    b. If the key doesn't exist in the map, create a new group with this string
+//    c. If the key exists, add this string to the existing group
+// 3. Return all groups from the map
 
 var groupAnagram = function (strs) {
   let map = {};
