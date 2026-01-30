b02dff4788f9732c9f9e62dd0d1b16617ef729f4
Rahul-5555 <rahul.malviya676@gmail.com>
2026-01-30 13:26:50 +0530

    add Queue

--- 8.Stack&Queves/2.Queue.js
+++ 8.Stack&Queves/2.Queue.js
@@ -1,1 +1,24 @@
 // Playing with Queue (FIFO - First In First Out)
+
+// Queue (FIFO - First In First Out)
+
+let queue = [];
+
+// ENQUEUE → add at end
+queue.push(10);
+queue.push(20);
+queue.push(30);
+
+console.log(queue); // [10, 20, 30]
+
+// DEQUEUE → remove from front
+queue.shift();
+console.log(queue); // [20, 30]
+
+// FRONT element
+let front = queue[0];
+console.log(front); // 20
+
+
+
+// NEVER DO THIS - DEQUEUE using pop (WRONG WAY) 
