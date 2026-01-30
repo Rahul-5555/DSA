4697ee6d5232e2545d29ca021009387b53f06398
Rahul-5555 <rahul.malviya676@gmail.com>
2026-01-30 12:56:06 +0530

    add Stack & Queue

--- 7.String/practice.js
+++ 7.String/practice.js
@@ -1,17 +1,11 @@
 
-var groupAnagrams = function (strs) {
-
+var isGroupAnagram = function(s,t){
   let map = {};
 
-  for (let i = 0; i < strs.length; i++) {
-    let sortedStr = strs[i].split('').sort().join('');
+  for(let i=0; i<s.length;i++){
 
-    if (!map[sortedStr]) {
-      map[sortedStr] = [strs[i]]
-    }
-    else {
-      map[sortedStr].push(strs[i]);
-    }
   }
-  return [...Object.values(map)];
 }
+
+let s = ["eat", "tea", "tan", "ate", "nat", "bat"]
+console.log(isGroupAnagram(s));
