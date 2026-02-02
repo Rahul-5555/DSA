52ee0e661cdee6cd880c877711471c8a5fbe6837
Rahul-5555 <rahul.malviya676@gmail.com>
2026-02-02 17:54:30 +0530

    modified leetcode 225 Question

--- 8.Stack&Queves/Q1.js
+++ 8.Stack&Queves/Q1.js
@@ -1,5 +1,14 @@
-//leetcode:225 Implement stack using queues
+//leetcode:225 Implement stack using two queues
 
 var MyStack = function(){
+  this.q1 = [];
+  this.q2 = [];
+}
+
+MyStack.prototype.push = function(x){
+  this.q1.push(x);
+};
+
+MyStack.prototype.pop = function(){
   
 }
