4240f0c8e0e75a587f620be6f8ea8c94d92d8994
Rahul-5555 <rahul.malviya676@gmail.com>
2026-02-11 11:32:09 +0530

    adding Leetcode 232 Que. implement Queue using stacks

--- 8.Stack&Queves/Q3.js
+++ 8.Stack&Queves/Q3.js
@@ -1,10 +1,32 @@
-// Implement Queue using Stacks
+// leetcode 232: Implement Queue using Stacks
 
-var MyQueue = function () {
-  this.stack1 = [];
-  this.stack2 = [];
+var MyQueue = function (x) {
+  this.s1 = [];
+  this.s2 = [];
 }
 
 MyQueue.prototype.push = function (x) {
-  this.stack1.push(x);
+  this.s1.push(x);
+}
+
+MyQueue.prototype.pop = function () {
+  if (this.s2.length === 0) {
+    while (this.s2.length) {
+      this.s2.push(this.s1.pop());
+    }
+  }
+  return this.s2.pop();
+}
+
+MyQueue.prototype.peek = function () {
+  if (this.s2.length === 0) {
+    while (this.s1.length) {
+      this.s2.push(this.s1.pop());
+    }
+  }
+  return this.s2[this.s2.length - 1];
+}
+
+MyQueue.prototype.peek = function () {
+  return this.s1.length === 0 && this.s2.length === 0;
 }
