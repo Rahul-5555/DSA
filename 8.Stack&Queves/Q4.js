74c875471586d3686a2617eee14f8b6bd2040013
Rahul-5555 <rahul.malviya676@gmail.com>
2026-02-12 13:39:17 +0530

    Add Q4 stack problem solution

---
+++ 8.Stack&Queves/Q4.js
@@ -0,0 +1,26 @@
+// leetcode 20:  Valid Parenttheses
+
+var isValid = function(s){
+  let stack = [];
+
+
+  for(let i=0; i<s.length;i++){
+    if(s[i] === '(' || s[i] === '{' || s[i] === '['){
+      stack.push(s[i]);
+    }
+    else {
+      let top = stack.pop();
+      if(!top || (top === "[" && s[i] !== "]") || (top === "{" && s[i] !== "}") || (top === "(" && s[i] !== ")")){
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
+}
+
+// test case
+console.log(isValid("()")); // true
+console.log(isValid("()[]{}")); // true
+console.log(isValid("(]")); // false
+console.log(isValid("([)]")); // false
+console.log(isValid("{[]}")); // true
