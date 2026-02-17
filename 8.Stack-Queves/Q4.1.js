baf54462e4501be8a39103767cf40980adff7dec
Rahul-5555 <rahul.malviya676@gmail.com>
2026-02-17 10:17:36 +0530

    improve solution Q4.1.js

--- 8.Stack-Queves/Q4.1.js
+++ 8.Stack-Queves/Q4.1.js
@@ -11,7 +11,7 @@ var isValid = function (s) {
 
   for (let i = 0; i < s.length; i++) {
     if (map[s[i]]) {
-    // if (s[i] === '(' || s[i] === '{' || s[i] === '[') {
+      // if (s[i] === '(' || s[i] === '{' || s[i] === '[') {
       stack.push(s[i]);
     }
     else {
--- 8.Stack-Queves/Q4.js
+++ 8.Stack-Queves/Q4.js
@@ -1,16 +1,16 @@
 // leetcode 20:  Valid Parenttheses
 
-var isValid = function(s){
+var isValid = function (s) {
   let stack = [];
 
 
-  for(let i=0; i<s.length;i++){
-    if(s[i] === '(' || s[i] === '{' || s[i] === '['){
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === '(' || s[i] === '{' || s[i] === '[') {
       stack.push(s[i]);
     }
     else {
       let top = stack.pop();
-      if(!top || (top === "[" && s[i] !== "]") || (top === "{" && s[i] !== "}") || (top === "(" && s[i] !== ")")){
+      if (!top || (top === "[" && s[i] !== "]") || (top === "{" && s[i] !== "}") || (top === "(" && s[i] !== ")")) {
         return false;
       }
     }
