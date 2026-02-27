70fedb4b86c5defc5cd80b7c768ee61435e5eecc
Rahul-5555 <rahul.malviya676@gmail.com>
2026-02-27 11:53:03 +0530

    add Q6.1.js Remove outermost parenthesis using stack improve code version

---
+++ 8.Stack-Queves/Q6.1.js
@@ -0,0 +1,22 @@
+// Remove outermost Parenthesis
+var removeOuterParentheses = function (s) {
+  let stack = [];
+  let ans = "";
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(") {
+      stack.push(s[i]);
+      ans += stack.length > 1 ? s[i] : "";
+    } else {
+      ans += stack.length > 1 ? s[i] : "";
+      stack.pop();
+    }
+
+  }
+  return ans;
+}
+
+// Example usage:
+const input = "(()())(())";
+const result = removeOuterParentheses(input);
+console.log(result); // Output: "()()()"
