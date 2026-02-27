253c109a4a5535f2c1dc9002e18d4996f383b562
Rahul-5555 <rahul.malviya676@gmail.com>
2026-02-27 11:45:56 +0530

    improve solution of Q6.js remove outermost parenthesis using queue

--- 8.Stack-Queves/Q6.js
+++ 8.Stack-Queves/Q6.js
@@ -1,1 +1,24 @@
 // Remove outermost Parenthesis
+var removeOuterParentheses = function (s) {
+    let stack = [];
+    let ans = "";
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === "(") {
+            stack.push(s[i]);
+            let len = stack.length;
+            if (len > 1) {
+                ans = ans + s[i];
+            }
+        } else {
+            let len = stack.length;
+            if (len > 1) {
+                ans = ans + s[i];
+            }
+            stack.pop();
+        }
+
+
+    }
+    return ans;
+}
